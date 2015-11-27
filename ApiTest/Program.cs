9c4e2ba7022a72daa85c7284557b4d203e247f00
Kocik <kocikowski11@gmail.com>
2015-11-28 00:50:08 +0100

    Test app init Register and Login

---
+++ ApiTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RestSharp;
+
+namespace ApiTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Testing /Register");
+			var client = new RestClient("http://qryptowire.azurewebsites.net/");
+			var request = new RestRequest("api/Register/asdasdad", Method.GET).AddParameter("deviceId", "sdasdasd");
+			// execute the request
+			var response = client.Execute(request);
+			Console.WriteLine(response.Content);
+			Console.WriteLine("Testing /Login");
+			client = new RestClient("http://qryptowire.azurewebsites.net/");
+			request = new RestRequest("api/Login/asdasdad", Method.GET).AddParameter("deviceId", "sdasdasd");
+			// execute the request
+			response = client.Execute(request);
+			Console.WriteLine(response.Content);
+		}
+	}
+}
