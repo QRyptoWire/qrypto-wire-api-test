a9524ca89d0ac4664f0724d4093d709ba0361f1a
Kocik <kocikowski11@gmail.com>
2015-11-28 03:46:51 +0100

    Response codes

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -7,12 +7,12 @@ namespace ApiTest
 	{
 		static void Main(string[] args)
 		{
-			bool LOCAL = true;
+			var LOCAL = false;
 			const string deviceId = "YrdJwOphEfbeDK3itnxSBa3Ldj0=";
 			const string password = "PASSWORD";
 			Console.WriteLine("Testing /Register");
-			var client = !LOCAL ? 
-				new RestClient("http://qryptowire.azurewebsites.net") 
+			var client = !LOCAL ?
+				new RestClient("http://qryptowire.azurewebsites.net")
 				: new RestClient("http://localhost:49954");
 
 			var request = new RestRequest("api/Register", Method.POST);
@@ -21,13 +21,27 @@ namespace ApiTest
 
 			// execute the request
 			var response = client.Execute(request);
+			Console.WriteLine(
+				"Status: "
+				+ response.ResponseStatus + " | "
+				+ response.StatusCode + " | "
+				+ response.ErrorMessage
+				);
 			Console.WriteLine(response.Content);
+
 			Console.WriteLine("Testing /Login");
 			request = new RestRequest("api/Login", Method.POST);
 			request.AddParameter("DeviceId", deviceId);
 			request.AddParameter("Password", password);
 			// execute the request
 			response = client.Execute(request);
+
+			Console.WriteLine(
+				"Status: " 
+				+ response.ResponseStatus + " | "
+				+ response.StatusCode + " | "
+				+ response.ErrorMessage
+				);
 			Console.WriteLine(response.Content);
 		}
 	}
