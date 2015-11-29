826df54d4b84d8ad4159b03be3ae4df2a94dddec
Kocik <kocikowski11@gmail.com>
2015-11-29 22:50:39 +0100

    Working with login, register, contacts and messages

---
+++ ApiTest/Classes/Contact.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Contact
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public int ReceiverId { get; set; }
+	public int SenderId { get; set; }
+	public string PublicKey { get; set; }
+}
--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -1,48 +1,167 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
 using RestSharp;
 
 namespace ApiTest
 {
 	class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			var LOCAL = false;
-			const string deviceId = "YrdJwOphEfbeDK3itnxSBa3Ldj0=";
-			const string password = "PASSWORD";
-			Console.WriteLine("Testing /Register");
-			var client = !LOCAL ?
-				new RestClient("http://qryptowire.azurewebsites.net")
+			var client = !LOCAL
+				? new RestClient("http://qryptowire.azurewebsites.net")
 				: new RestClient("http://localhost:49954");
 
-			var request = new RestRequest("api/Register", Method.POST);
-			request.AddParameter("DeviceId", deviceId);
-			request.AddParameter("Password", password);
+			RestRequest request;
+			IRestResponse response;
 
-			// execute the request
-			var response = client.Execute(request);
-			Console.WriteLine(
-				"Status: "
-				+ response.ResponseStatus + " | "
-				+ response.StatusCode + " | "
-				+ response.ErrorMessage
-				);
-			Console.WriteLine(response.Content);
+			string deviceId;
+			string password;
+			string sessionId = "";
 
+			Console.WriteLine("DeviceId:");
+			deviceId = Console.ReadLine();
+			if (deviceId == "")
+			{
+				deviceId = "asd";
+			}
+			Console.WriteLine("Password:");
+			password = Console.ReadLine();
+			if (password == "")
+			{
+				password = "PASSWORD";
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Test /Register");
+			if (Console.ReadLine() != "n")
+			{
+				request = new RestRequest("api/Register", Method.POST);
+				request.AddParameter("DeviceId", deviceId);
+				request.AddParameter("Password", password);
+
+				// execute the request
+				response = client.Execute(request);
+				Console.WriteLine(
+					"Status: "
+					+ response.ResponseStatus + " | "
+					+ response.StatusCode + " | "
+					+ response.ErrorMessage
+					);
+				Console.WriteLine(response.Content);
+			}
+
+			Console.WriteLine();
 			Console.WriteLine("Testing /Login");
-			request = new RestRequest("api/Login", Method.POST);
-			request.AddParameter("DeviceId", deviceId);
-			request.AddParameter("Password", password);
-			// execute the request
-			response = client.Execute(request);
+			if (Console.ReadLine() != "n")
+			{
+				request = new RestRequest("api/Login", Method.POST);
+				request.AddParameter("DeviceId", deviceId);
+				request.AddParameter("Password", password);
+				// execute the request
+				response = client.Execute(request);
 
-			Console.WriteLine(
-				"Status: " 
-				+ response.ResponseStatus + " | "
-				+ response.StatusCode + " | "
-				+ response.ErrorMessage
-				);
-			Console.WriteLine(response.Content);
+				Console.WriteLine(
+					"Status: "
+					+ response.ResponseStatus + " | "
+					+ response.StatusCode + " | "
+					+ response.ErrorMessage
+					);
+				var a = new char[1];
+				a[0] = '\"';
+				sessionId = response.Content.Trim(a);
+				Console.WriteLine(sessionId);
+			}
+
+
+			Console.WriteLine();
+			Console.WriteLine("Testing /SendMessage");
+			if (Console.ReadLine() != "n")
+			{
+				request = new RestRequest($"api/SendMessage/{sessionId}", Method.POST);
+				var msg = new Message()
+				{
+					Body = "ARAGoRn",
+					DateSent = DateTime.Now,
+					ReceiverId = 171,
+					SenderId = 171,
+					SessionKey = "aaa",
+					Signature = "bbb",
+					Time = DateTime.Now
+				};
+				request.AddJsonBody(msg);
+				response = client.Execute(request);
+
+
+				Console.WriteLine(
+					"Status: "
+					+ response.ResponseStatus + " | "
+					+ response.StatusCode + " | "
+					+ response.ErrorMessage
+					);
+				Console.WriteLine(response.Content);
+			}
+
+
+			Console.WriteLine();
+			Console.WriteLine("Testing /FetchMessages");
+			if (Console.ReadLine() != "n")
+			{
+				request = new RestRequest($"api/FetchMessages/{sessionId}", Method.GET);
+				response = client.Execute(request);
+
+				Console.WriteLine(
+					"Status: "
+					+ response.ResponseStatus + " | "
+					+ response.StatusCode + " | "
+					+ response.ErrorMessage
+					);
+				Console.WriteLine(response.Content);
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Testing /AddContact");
+			if (Console.ReadLine() != "n")
+			{
+				request = new RestRequest($"api/AddContact/{sessionId}", Method.POST);
+				var con = new Contact()
+				{
+					ReceiverId = 171,
+					SenderId = 171,
+					Name = "Papa Smurf",
+					PublicKey = "keypub"
+				};
+				request.AddJsonBody(con);
+				response = client.Execute(request);
+
+				Console.WriteLine(
+					"Status: "
+					+ response.ResponseStatus + " | "
+					+ response.StatusCode + " | "
+					+ response.ErrorMessage
+					);
+				Console.WriteLine(response.Content);
+			}
+
+
+			Console.WriteLine();
+			Console.WriteLine("Testing /FetchContacts");
+			if (Console.ReadLine() != "n")
+			{
+
+				request = new RestRequest($"api/FetchContacts/{sessionId}", Method.GET);
+				response = client.Execute(request);
+
+				Console.WriteLine(
+					"Status: "
+					+ response.ResponseStatus + " | "
+					+ response.StatusCode + " | "
+					+ response.ErrorMessage
+					);
+				Console.WriteLine(response.Content);
+			}
+
 		}
 	}
 }
